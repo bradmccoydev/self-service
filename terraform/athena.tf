946b43eba81b2fb2a40a049e14537dbab8e5ca57
bradmccoydev <bradmccoydev@gmail.com>
2020-10-13 20:35:46 +1100

    ADP-217 added athena for logs and metrics

--- terraform/athena.tf
+++ terraform/athena.tf
@@ -1,24 +1,29 @@
-# resource "aws_athena_workgroup" "athena_workgroup" {
-#   name = "athena_workgroup"
+resource "aws_athena_workgroup" "athena_workgroup" {
+  name = "athena_workgroup"
 
-#   configuration {
-#     result_configuration {
-#       encryption_configuration {
-#         encryption_option = "SSE_KMS"
-#         kms_key_arn       = aws_kms_key.athena_kms_key.arn
-#       }
-#     }
-#   }
-# }
+  configuration {
+    result_configuration {
+      encryption_configuration {
+        encryption_option = "SSE_KMS"
+        kms_key_arn       = aws_kms_key.athena_kms_key.arn
+      }
+    }
+  }
+}
 
-# resource "aws_athena_database" "athena_logs" {
-#   name   = "logs"
-#   bucket = aws_s3_bucket.terraform_state.id
-# }
+resource "aws_athena_database" "logs" {
+  name   = "logs"
+  bucket = aws_s3_bucket.logging.bucket
+}
 
-# resource "aws_athena_named_query" "athena_query" {
-#   name      = "query"
-#   workgroup = aws_athena_workgroup.athena_workgroup.id
-#   database  = aws_athena_database.athena_logs.name
-#   query     = "SELECT * FROM ${aws_athena_database.athena_logs.name} limit 10;"
-# }
+resource "aws_athena_database" "metrics" {
+  name   = "metrics"
+  bucket = aws_s3_bucket.metrics.bucket
+}
+
+resource "aws_athena_named_query" "athena_query" {
+  name      = "query"
+  workgroup = aws_athena_workgroup.athena_workgroup.id
+  database  = aws_athena_database.logs.name
+  query     = "SELECT * FROM ${aws_athena_database.logs.name} limit 10;"
+}
