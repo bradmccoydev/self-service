be91ba613c029d43f7b0ff4946a00e60ed042623
bradmccoydev <bradmccoydev@gmail.com>
2020-10-16 09:50:52 +1100

    ADP-217 added cognito iam roles

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -8,7 +8,7 @@ resource "aws_cognito_identity_pool" "main" {
   allow_unauthenticated_identities = false
 
   cognito_identity_providers {
-    client_id               = "${aws_cognito_user_pool_client.web.id}"
+    client_id               = aws_cognito_user_pool_client.web.id
     provider_name           = "cognito-idp.${var.aws_region}.amazonaws.com/${aws_cognito_user_pool.main.id}"
     server_side_token_check = false
   }
@@ -100,7 +100,7 @@ resource "aws_cognito_user_group" "manager" {
 
 
 resource "aws_cognito_user_pool_client" "web" {
-  name         = "${var.namespace}-client-web"
+  name         = "${var.application_name}-client-web"
   user_pool_id = "${aws_cognito_user_pool.main.id}"
 }
 
