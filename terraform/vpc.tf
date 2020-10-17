de1cc3d754e8324ab2d92024c443e94d0f22458f
bradmccoydev <bradmccoydev@gmail.com>
2020-10-18 10:30:32 +1100

    self-service-05 added elastic ip

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -67,12 +67,12 @@ resource "aws_eip" "nat" {
 }
 
 resource "aws_route_table_association" "private_1" {
-  subnet_id      = aws_subnet.private_1
+  subnet_id      = aws_subnet.private_1.id
   route_table_id = aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "private_2" {
-  subnet_id      = aws_subnet.private_2
+  subnet_id      = aws_subnet.private_2.id
   route_table_id = aws_route_table.private.id
 }
 
