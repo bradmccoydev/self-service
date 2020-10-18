ea41c562f7a7f02dae7778481614e7f20eb8360e
amit dsouza <>
2020-10-18 20:06:38 +1100

    Add ECS Fargate support

---
+++ terraform/ecs.tf
@@ -0,0 +1,17 @@
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = var.ecs_cluster_name
+}
+
+
+resource "aws_ecs_task_definition" "ecs_task_definition" {
+  family                   = var.ecs_task_definition
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  network_mode             = "awsvpc"
+  cpu                      = "1024"
+  memory                   = "1024"
+  requires_compatibilities = ["FARGATE"]
+  }
+
+
+
--- terraform/iam.tf
+++ terraform/iam.tf
@@ -613,3 +613,96 @@ EOF
 # }
 # EOF
 # }
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ECS
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = var.ecs_task_execution_role
+ 
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "ecs-tasks.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "role-name-task"
+ 
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "ecs-tasks.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+ 
+resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+resource "aws_iam_role_policy_attachment" "ecs_task" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_role_policy.ecs_task_policy.arn
+}
+
+resource "aws_iam_role_policy" "ecs_task_policy" {
+  name = "selfservice-ecs-fargate-task-policy"
+  role = aws_iam_role.ecs_task_role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "secretsmanager:GetSecretValue",
+        "cloudwatch:PutMetricData",
+        "sqs:*",
+        "ses:SendEmail",
+        "ses:SendRawEmail"
+      ],
+      "Resource": [
+        "*"
+      ]
+    },
+    {
+      "Action": [
+          "dynamodb:DescribeTable",
+          "dynamodb:GetRecords",
+          "dynamodb:GetItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:Scan",
+          "dynamodb:Query"
+      ],
+      "Resource": "arn:aws:dynamodb:${var.aws_region}:*",
+      "Effect": "Allow"
+    },
+  ]
+}
+EOF
+}
+
+//ECS task policy to be decided
