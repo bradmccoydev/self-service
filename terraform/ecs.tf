e1daac7d236846ca33cb330de73e86561bc8c267
bradmccoydev <bradmccoydev@gmail.com>
2020-10-29 11:34:48 +1100

    SS-18 Removed fargate

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,17 +1,3 @@
-resource "aws_ecs_cluster" "ecs_cluster" {
-  name = var.ecs_cluster_name
-}
-
-
-# resource "aws_ecs_task_definition" "ecs_task_definition" {
-#   family                   = var.ecs_task_definition
-#   task_role_arn            = aws_iam_role.ecs_task_role.arn
-#   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-#   network_mode             = "awsvpc"
-#   cpu                      = "1024"
-#   memory                   = "1024"
-#   requires_compatibilities = ["FARGATE"]
-#   }
-
-
-
+# resource "aws_ecs_cluster" "ecs_cluster" {
+#   name = var.ecs_cluster_name
+# }
--- terraform/iam.tf
+++ terraform/iam.tf
@@ -634,88 +634,88 @@ resource "aws_iam_role_policy_attachment" "appsync" {
 # ECS
 # ---------------------------------------------------------------------------------------------------------------------
 
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = var.ecs_task_execution_role
+# resource "aws_iam_role" "ecs_task_execution_role" {
+#   name = var.ecs_task_execution_role
  
-  assume_role_policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-   {
-     "Action": "sts:AssumeRole",
-     "Principal": {
-       "Service": "ecs-tasks.amazonaws.com"
-     },
-     "Effect": "Allow",
-     "Sid": ""
-   }
- ]
-}
-EOF
-}
+#   assume_role_policy = <<EOF
+# {
+#  "Version": "2012-10-17",
+#  "Statement": [
+#    {
+#      "Action": "sts:AssumeRole",
+#      "Principal": {
+#        "Service": "ecs-tasks.amazonaws.com"
+#      },
+#      "Effect": "Allow",
+#      "Sid": ""
+#    }
+#  ]
+# }
+# EOF
+# }
 
-resource "aws_iam_role" "ecs_task_role" {
-  name = "role-name-task"
+# resource "aws_iam_role" "ecs_task_role" {
+#   name = "role-name-task"
  
-  assume_role_policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-   {
-     "Action": "sts:AssumeRole",
-     "Principal": {
-       "Service": "ecs-tasks.amazonaws.com"
-     },
-     "Effect": "Allow",
-     "Sid": ""
-   }
- ]
-}
-EOF
-}
+#   assume_role_policy = <<EOF
+# {
+#  "Version": "2012-10-17",
+#  "Statement": [
+#    {
+#      "Action": "sts:AssumeRole",
+#      "Principal": {
+#        "Service": "ecs-tasks.amazonaws.com"
+#      },
+#      "Effect": "Allow",
+#      "Sid": ""
+#    }
+#  ]
+# }
+# EOF
+# }
  
-resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-resource "aws_iam_role_policy_attachment" "ecs_task" {
-  role       = aws_iam_role.ecs_task_role.name
-  policy_arn = aws_iam_policy.ecs_task_policy.arn
-}
+# resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
+#   role       = aws_iam_role.ecs_task_execution_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+# }
+# resource "aws_iam_role_policy_attachment" "ecs_task" {
+#   role       = aws_iam_role.ecs_task_role.name
+#   policy_arn = aws_iam_policy.ecs_task_policy.arn
+# }
 
-resource "aws_iam_policy" "ecs_task_policy" {
-  name = "selfservice-fargate-task"
+# resource "aws_iam_policy" "ecs_task_policy" {
+#   name = "selfservice-fargate-task"
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "secretsmanager:GetSecretValue",
-        "cloudwatch:PutMetricData",
-        "sqs:*",
-        "ses:SendEmail",
-        "ses:SendRawEmail"
-      ],
-      "Resource": [
-        "*"
-      ]
-    },
-    {
-      "Action": [
-          "dynamodb:DescribeTable",
-          "dynamodb:GetRecords",
-          "dynamodb:GetItem",
-          "dynamodb:UpdateItem",
-          "dynamodb:Scan",
-          "dynamodb:Query"
-      ],
-      "Resource": "arn:aws:dynamodb:${var.aws_region}:*",
-      "Effect": "Allow"
-    }
-  ]
-}
-EOF
-}
+#   policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Action": [
+#         "secretsmanager:GetSecretValue",
+#         "cloudwatch:PutMetricData",
+#         "sqs:*",
+#         "ses:SendEmail",
+#         "ses:SendRawEmail"
+#       ],
+#       "Resource": [
+#         "*"
+#       ]
+#     },
+#     {
+#       "Action": [
+#           "dynamodb:DescribeTable",
+#           "dynamodb:GetRecords",
+#           "dynamodb:GetItem",
+#           "dynamodb:UpdateItem",
+#           "dynamodb:Scan",
+#           "dynamodb:Query"
+#       ],
+#       "Resource": "arn:aws:dynamodb:${var.aws_region}:*",
+#       "Effect": "Allow"
+#     }
+#   ]
+# }
+# EOF
+# }
